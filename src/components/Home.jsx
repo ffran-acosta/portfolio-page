5267418a7de50db03c39417cb8db1380ff6750b7
Franco Acosta <francoaugustoacosta@gmail.com>
2023-03-15 20:22:37 -0300

    home detail

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,11 +6,11 @@ const Home = () => {
     return (
         <div name='home' className='w-full h-screen bg-[#112b51]'>
 
-            <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+            <div className='max-w-[1000px] mx-auto px-8 pt-[100px] flex flex-col justify-center h-full'>
                 <p className='text-[#ef913e] sm:text-2xl'>Hi! My name is</p>
-                <h1 className='text-7xl sm:text-7xl font-bold text-white'>Franco</h1>
-                <h2 className='text-4xl sm:text-6xl font-bold text-white'>I'm Full Stack Web Developer<span className='text-[#ef913e]'>.</span></h2>
-                <p className='text-white py-6 max-w-[700px] text-xl text-justify'>I'm looking for my first professional opportunity in the industry. My main focus is on building responsive and user-friendly websites using the latest web technologies such as HTML5, CSS3, React, and TypeScript. I have expertise in using Node.js and Express.js for server-side development and APIs. I believe in writing clean and efficient code, and I'm always up to date with the latest development trends and technologies. I have experience working with various databases such as MySQL, MongoDB, and PostgreSQL. My goal is to deliver high-quality web solutions that help businesses achieve their online objectives.</p>
+                <h1 className='text-5xl sm:text-7xl font-bold text-white'>Franco</h1>
+                <h2 className='text-3xl sm:text-4xl font-bold text-white'>I'm Full Stack Web Developer<span className='text-[#ef913e]'>.</span></h2>
+                <p className='text-white py-6 max-w-[700px] text-sm sm:text-xl text-justify'>I'm looking for my first professional opportunity in the industry. My main focus is on building responsive and user-friendly websites using the latest web technologies such as HTML5, CSS3, React, and TypeScript. I have expertise in using Node.js and Express.js for server-side development and APIs. I believe in writing clean and efficient code, and I'm always up to date with the latest development trends and technologies. I have experience working with various databases such as MySQL, MongoDB, and PostgreSQL. My goal is to deliver high-quality web solutions that help businesses achieve their online objectives.</p>
                 <div>
                     <Link to="work" smooth={true} duration={500} >
                     <button className=' bg-[#102649] text-[#ef913e] group border-2 border-[#ef913e] px-6 py-3 my-2 flex items-center hover:bg-[#ef913e] hover:text-white duration-300'> 
